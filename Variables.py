8d85a06c7518105cf8a98275d5cc19192897420f
smartifycontent-Tahsin <smartifycontent@gmail.com>
2025-12-06 20:17:59 +0600

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,33 @@
+'''
+x= 4 # it is a integer
+x= "Tahsin" #it is a string
+x= 5.5 # it is a float
+print(x)
+'''
+
+"""
+print a sobsomoy last ar ta ashbe
+"""
+
+# legal variable
+'''
+myname = "Tahsin"
+my_name= "Tahsin"
+MyName= "Tahsin"
+MyNAME= "Tahsin"
+'''
+
+# illegal variable
+'''
+2myname= "Tahsin"
+my-name= "Tahsin"
+my name= "Tahsin"
+'''
+
+# Variable declaration
+x,y,z = "Tahsin","Fardin","Rafi" # Ai bhabao variable declare kora jai
+x=y=z= "Tahsin"
+p= 18
+print(f"My name is {x} and I am {p} years old")
+
+
