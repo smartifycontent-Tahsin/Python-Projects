a5453ed83ec6042efe5b1b9d60e25c898f08550b
smartifycontent-Tahsin <smartifycontent@gmail.com>
2025-12-06 20:17:27 +0600

    Add files via upload

---
+++ Comments.py
@@ -0,0 +1,6 @@
+print("How to do a comment?")
+# Comment kora hoi onno developer jeno khub sohojai dekhe code ti bujte pare
+"""
+Akadhik line a comment korar korar jonno
+triple inverted comma use kora hoi
+"""
